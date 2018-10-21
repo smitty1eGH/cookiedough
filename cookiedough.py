a4192fc2ddf52f99a9428b648c0391e95af206db
Smith, Christopher <smith_christopher@bah.com>
2018-10-21 07:59:25 -0400

    first baby step

--- cookiedough.py
+++ cookiedough.py
@@ -1,4 +1,6 @@
 import os
+import json
+from pathlib import Path
 import subprocess
 import click
 
@@ -17,4 +19,21 @@ def dtls():
     CMD="pip install --upgrade --user black mypy pytest sphinx"
     subprocess.run(CMD.split(),env=os.environ)
 
+@click.command()
+def cc():
+    '''Run the cookie cutter.
+    '''
+    #TODO: need to verify the directory doesn't exist and provide an override.
+    ccj=None
+    with open('cookiecutter.json','r') as f:
+        ccj=json.loads(f.read())
+    p = Path('/home/smitty/proj/%s' % ccj["project_slug"])
+    if p.exists():
+        print("need to blow this away if desired")
+    else:
+        print("building %s" % p)
+        CMD="python -m venv %s" % p
+        subprocess.run(CMD.split(),env=os.environ)
+
 cli.add_command(dtls)
+cli.add_command(cc)
